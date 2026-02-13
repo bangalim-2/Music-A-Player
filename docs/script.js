df4c7dc8c805d767afc9f42d4cb11aafb0c0968b
momodu <momodu@momodus-MacBook-Pro.local>
2026-02-13 03:43:12 -0500

    Publish Music-A-Player to GitHub Pages (docs)

---
+++ docs/script.js
@@ -0,0 +1,73 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+const songs = ["Can't Take My Eyes Off Of You", 'Great Day', 'Riot'];
+let songIndex = 1;
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${encodeURIComponent(song)}.mp3`;
+  cover.src = `music/images/${encodeURIComponent(song)}.jpg`;
+}
+
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.replace('fa-play', 'fa-pause');
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.replace('fa-pause', 'fa-play');
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex = (songIndex - 1 + songs.length) % songs.length;
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex = (songIndex + 1) % songs.length;
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const percent = (currentTime / duration) * 100;
+  progress.style.width = `${percent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+audio.addEventListener('timeupdate', updateProgress);
+progressContainer.addEventListener('click', setProgress);
+audio.addEventListener('ended', nextSong);
+
+loadSong(songs[songIndex]);
